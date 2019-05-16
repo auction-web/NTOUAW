690bb5a1c05dd906979f53bacba9c33e09c4d0a4
chen <t5689123@gmail.com>
2019-05-16 18:27:47 +0800

    update login

--- js/login.js
+++ js/login.js
@@ -1,10 +1,28 @@
+var firebase= require("./firebase");
+var db = firebase.firestore();
+var citiesRef = db.collection('User23');
+
+
+
 var cook=getCookie('id');
-
-
-if(cook!=""){
-	location.href = "./home.html";
-}
-
+//alert(cook);
+var allCities = citiesRef.where('user_id', '==',cook ).get()
+	 .then(snapshot => {
+		// alert(snapshot.empty);
+		if (snapshot.empty) {
+			//console.log('No matching documents.');
+			//alert("帳密錯誤");
+			//location.href = "./index.html";
+			//alert("123");
+		} 
+		else{
+			//alert("3456");
+			location.href = "./home.html";
+		}
+	})
+	.catch(err => {
+		console.log('Error getting documents', err);
+	});
 login=function(){
 	 var Account_element = document.getElementById('Account');
 	 var Account= Account_element.value;
@@ -16,9 +34,9 @@ login=function(){
 		 // console.log('Document data1111:');
 		  return ;
 	 }
-	  var firebase= require("./firebase");
+	  /*var firebase= require("./firebase");
 	  var db = firebase.firestore();
-	  var citiesRef = db.collection('User23');
+	  var citiesRef = db.collection('User23');*/
   
 	 var allCities = citiesRef.where('account', '==',Account ).where('password', '==',Password ).get()
 	 .then(snapshot => {
