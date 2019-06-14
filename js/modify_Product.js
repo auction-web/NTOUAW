f2a33cf14f91e19804443ec89f7dd1827ad3ac66
chen <t5689123@gmail.com>
2019-06-15 00:24:43 +0800

    update modify

--- js/modify_Product.js
+++ js/modify_Product.js
@@ -6,7 +6,6 @@ var input = document.getElementById("file_input");
 var input1 = document.getElementById("file_input1"); 
 var input2 = document.getElementById("file_input2"); 
 var input3 = document.getElementById("file_input3"); 
-var input4 = document.getElementById("file_input4"); 
 if(typeof FileReader==='undefined'){ 
 	result.innerHTML = "Sorry, 瀏覽器不支持 FileReader"; 
 	input.setAttribute('disabled','disabled'); 
@@ -15,7 +14,6 @@ if(typeof FileReader==='undefined'){
 	input1.addEventListener('change',readFile1,false);
 	input2.addEventListener('change',readFile2,false);
 	input3.addEventListener('change',readFile3,false);
-	input4.addEventListener('change',readFile4,false);
 }
 var aa;
 var url = location.href; // product-detail.html
@@ -24,16 +22,17 @@ aa = (url.split('?')[1]).split('=')[1];
 //console.log('id = ',aa);
 /*var User='User23/';
 var User1='User7';
+var product_id=45;*/
 //console.log(product_id);*/
 var product_id=Number(aa);
 var cook=getCookie('id');
 var User1='User'+cook;
 var User=User1+'/';
 
-/*if(cook==""){
+if(cook==""){
 	alert("請先登入，跳轉至登入頁面");
 	location.href = "./index.html";
-}*/
+}
 //var today=new Date();
 //alert(today);
 
@@ -201,49 +200,6 @@ function readFile3(){
 	  // Handle any errors
 	});*/
 }
-function readFile4(){	
-	var file = this.files[0]; 
-	//alert(this.files.length);
-	var reader = new FileReader(); 
-	//var bigImg = document.createElement("img");
-	reader.readAsDataURL(file); 
-	reader.onload = function(e){ 
-		//result.innerHTML = '<img src="'+this.result+'" alt="" height="100" />' ;
-		//var bigImg = document.createElement("img");
-		//bigImg.src=this.result;
-		var myDiv4 = document.getElementById('myDiv4'); //获得dom对象
-		//myDiv.appendChild(bigImg);
-		myDiv4.src=this.result;
-		//console.log(file);
-	} 
-		//var firebase= require("./firebase");
-		//var db = firebase.firestore();
-		//var storageRef = firebase.storage().ref();
-		var Counter = db.collection('Counter').doc('Product');
-		var uploadTask = storageRef.child('Products/'+'Products'+product_id.toString()+'/4').put(file);				
-
-	
-	/*storageRef.child('images/55').getDownloadURL().then(function(url) {
-	  // `url` is the download URL for 'images/stars.jpg'
-	  console.log(url);
-	  // This can be downloaded directly:
-	  var xhr = new XMLHttpRequest();
-	  xhr.responseType = 'blob';
-	  xhr.onload = function(event) {
-		var blob = xhr.response;
-	  };
-	  xhr.open('GET', url);
-	  xhr.send();
-	  var myDiv = document.getElementById('myDiv'); //获得dom对象
-	  
-	  // Or inserted into an <img> element:
-	  var img = document.createElement('img');
-	  img.src = url;
-	  myDiv.appendChild(img);
-	}).catch(function(error) {
-	  // Handle any errors
-	});*/
-}
 var category_element= document.getElementById("category");
 var criticize_element= document.getElementById("criticize");
 var price_element= document.getElementById("price");
@@ -262,22 +218,29 @@ var getDoc = citiesRef.get()
 			} else {
 				console.log('Document data:', doc.data());
 			}
-				criticize_element.innerText="評價: "+doc.data()['product_evaluation'].toString();
+				criticize_element.innerText="評價: "+doc.data()['product_evaluation'];
 				category_element.options[doc.data()['product_kind']].selected=true;
+				/*category_element.innerHTML = '';
 				
+				var sel=['','','','','','','','','',''];
+				sel[0]='selected';
+				category_element.innerHTML='<option value="0"'+sel[0]+' >書籍文具</option>'+
+                            '<option value="1"'+sel[1]+' >衣物配飾</option>'+
+                            '<option value="2"'+sel[2]+'>廚房用品</option>'+
+                            '<option value="3"'+ sel[3]+'>3c週邊</option>'+
+                            '<option value="4"'+sel[4]+'>運動用品</option>'+
+                            '<option value="5"'+sel[5]+'>美味食品</option>'+
+                            '<option value="6"'+sel[6]+'>美妝保養</option>'+
+                            '<option value="7"'+sel[7]+'>家俱家電</option>'+
+                            '<option value="8"'+sel[8]+'>其他類</option>';*/
 				//console.log(category_element.options[category_element.selectedIndex].value);
 				price_element.value=doc.data()['price'];
 				product_element.value=doc.data()['product_title'];
 				var show = document.getElementById('tex');
 				show.innerHTML = '';
-				var x=doc.data()['is_Bid']?'競標':'一般';
-				show.innerHTML="<select name='bidorUnbid'>"+"<option value='Bid'>"+x+'</option>'+'</select>';
-								
-								
-								
-
-			
-									 
+				var x=doc.data()['is_Bid']?'競標商品':'一般商品';
+				show.innerHTML="<p>"+x+'</p>';
+					 
 				introduction_element.value=doc.data()['product_intro'];	
 				if(doc.data()['delivery'][0]==true){
 					obj[0].checked=true;
@@ -360,23 +323,6 @@ upload =function() {
 
 	  // Or inserted into an <img> element:
 	  var img = document.getElementById('myDiv3');
-	  img.src = url;
-	}).catch(function(error) {
-	  // Handle any errors
-	});
-	storageRef.child('Products/'+'Products'+product_id.toString()+'/4').getDownloadURL().then(function(url) {
-	  //// `url` is the download URL for 'images/stars.jpg'
-	  // This can be downloaded directly:
-//	  var xhr = new XMLHttpRequest();
-//	  xhr.responseType = 'blob';
-//	  xhr.onload = function(event) {
-//		var blob = xhr.response;
-//	  };
-//	  xhr.open('GET', url);
-//	  xhr.send();
-
-	  // Or inserted into an <img> element:
-	  var img = document.getElementById('myDiv4');
 	  img.src = url;
 	}).catch(function(error) {
 	  // Handle any errors
