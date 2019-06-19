e448a77da713390ed8e830a80ec44574ce32624a
chen <t5689123@gmail.com>
2019-06-20 00:56:48 +0800

    update picture

--- js/personal1.js
+++ js/personal1.js
@@ -52562,13 +52562,13 @@ storageRef.child('Users/'+User+'picture').getDownloadURL().then(function(url) {
   //// `url` is the download URL for 'images/stars.jpg'
 
   // This can be downloaded directly:
-  var xhr = new XMLHttpRequest();
+  /*var xhr = new XMLHttpRequest();
   xhr.responseType = 'blob';
   xhr.onload = function(event) {
     var blob = xhr.response;
   };
   xhr.open('GET', url);
-  xhr.send();
+  xhr.send();*/
 
   // Or inserted into an <img> element:
   var img = document.getElementById('myDiv');
